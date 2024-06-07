c8340aa24f42e2aa20119d55409ccd46337d066e
Abdulrhman Ashraf <89071774+AbdulrahmanAshraf98@users.noreply.github.com>
2024-06-07 06:53:34 +0300

    Time: 9 ms (89.76%) | Memory: 61.1 MB (5.39%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,17 +1,17 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        // HashSet<Integer> nonDuplicateNumber = new HashSet<>();
-        // for(int num:nums){
-        //     if(!nonDuplicateNumber.add(num))
-        //         return true;
-        // }
-        // return false;
-        Arrays.sort(nums);
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] == nums[i + 1]) {
+        HashSet<Integer> nonDuplicateNumber = new HashSet<>();
+        for(int num:nums){
+            if(!nonDuplicateNumber.add(num))
                 return true;
-            }
         }
         return false;
+        // Arrays.sort(nums);
+        // for (int i = 0; i < nums.length - 1; i++) {
+        //     if (nums[i] == nums[i + 1]) {
+        //         return true;
+        //     }
+        // }
+        // return false;
     }
 }
