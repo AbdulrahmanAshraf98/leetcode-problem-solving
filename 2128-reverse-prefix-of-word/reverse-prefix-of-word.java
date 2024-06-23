5064914072d957e72e1c71df854637bac78dc2d6
Abdulrhman Ashraf <89071774+AbdulrahmanAshraf98@users.noreply.github.com>
2024-06-23 18:29:30 +0300

    Time: 0 ms (100.00%) | Memory: 41.8 MB (19.40%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.java
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.java
@@ -1,15 +1,10 @@
 class Solution {
     public String reversePrefix(String word, char ch) {
-          int index  =-1;
-        for(int i = 0 ; i<word.length();i++){
-            if(word.charAt(i)==ch){
-                index =i;
-                break;
-            }
-        }
+       int index = word.indexOf(ch);
         if(index==-1)return  word;
         char[] arr = word.toCharArray();
         for (int i = 0; i <= index / 2; i++) {
+            // Swap characters at indices i and index - i
             char temp = arr[i];
             arr[i] = arr[index - i];
             arr[index - i] = temp;
