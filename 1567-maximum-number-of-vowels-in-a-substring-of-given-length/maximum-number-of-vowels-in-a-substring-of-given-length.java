f64c4f8ecb8c034e0a76256a3800e472567212e3
Abdulrhman Ashraf <89071774+AbdulrahmanAshraf98@users.noreply.github.com>
2024-07-01 17:35:33 +0300

    Time: 8 ms (96.64%) | Memory: 44.2 MB (85.67%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,21 +1,20 @@
 class Solution {
    public int maxVowels(String s, int k) {
-        Set<Character> vowelsCharacter = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
+        int[] isVowel = new int[26];
+        isVowel['a' - 'a'] = 1;
+        isVowel['e' - 'a'] = 1;
+        isVowel['i' - 'a'] = 1;
+        isVowel['o' - 'a'] = 1;
+        isVowel['u' - 'a'] = 1;        
         int maxCount = 0;
         int currentCount = 0;
         for (int i = 0; i < k; i++) {
-            if (vowelsCharacter.contains(s.charAt(i))) {
-                currentCount++;
-            }
+            currentCount += isVowel[s.charAt(i) - 'a'];
         }
         maxCount = currentCount;
         for (int i = k; i < s.length(); i++) {
-            if (vowelsCharacter.contains(s.charAt(i - k))) {
-                currentCount--;
-            }
-            if (vowelsCharacter.contains(s.charAt(i))) {
-                currentCount++;
-            }
+            currentCount -= isVowel[s.charAt(i - k) - 'a'];
+            currentCount += isVowel[s.charAt(i) - 'a'];
             maxCount = Math.max(maxCount, currentCount);
         }
         return maxCount;
