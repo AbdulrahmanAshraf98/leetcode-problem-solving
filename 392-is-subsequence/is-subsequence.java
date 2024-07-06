b0c59f9e0bdb7cdb9d3f38ea3cdd6b883cb44df4
Abdulrhman Ashraf <89071774+AbdulrahmanAshraf98@users.noreply.github.com>
2024-07-06 14:30:13 +0300

    Time: 1 ms (92.42%) | Memory: 41.4 MB (26.47%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,30 +1,18 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        Map<Character, List<Integer>> map = new HashMap<>();
-        // Preprocess t
-        for (int i = 0; i < t.length(); i++) {
-            char c = t.charAt(i);
-            if (!map.containsKey(c)) {
-                map.put(c, new ArrayList<>());
+        if(s.length()==0)return true;
+        int i = 0 ; 
+        int j = 0 ; 
+        int sLength=s.length();
+        int tLength=t.length();
+        while(i<tLength&&j<sLength){
+            if(t.charAt(i)==s.charAt(j)){
+                if(sLength==j)return true ;
+                j++;
             }
-            map.get(c).add(i);
+            i++;
         }
+        return sLength==j;
 
-        int prev = -1;  // The previous character's index
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (!map.containsKey(c)) {
-                return false;
-            }
-            int j = Collections.binarySearch(map.get(c), prev + 1);
-            if (j < 0) {
-                j = -j - 1;
-            }
-            if (j == map.get(c).size()) {
-                return false;
-            }
-            prev = map.get(c).get(j);
-        }
-        return true;
     }
 }
