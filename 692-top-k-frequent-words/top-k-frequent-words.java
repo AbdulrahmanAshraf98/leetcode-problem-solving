6da11ca5be9a136d97b11fbf8ffa3aaa129f8c4f
Abdulrhman Ashraf <89071774+AbdulrahmanAshraf98@users.noreply.github.com>
2024-07-07 11:14:30 +0300

    Time: 7 ms (68.36%) | Memory: 44.9 MB (8.44%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.java
+++ 692-top-k-frequent-words/top-k-frequent-words.java
@@ -1,14 +1,16 @@
 class Solution {
-     HashMap<String,Integer> frequentWords= new HashMap<>();   
+    HashMap<String,Integer> frequentWords;
+    List<String> candidates;
     public List<String> topKFrequent(String[] words, int k) {
          if(words == null || words.length == 0) {
             return new ArrayList<String>();
         }
+        frequentWords= new HashMap<>();   
         for(String word : words ){
             if(frequentWords.get(word)!=null)frequentWords.put(word,frequentWords.get(word)+1);
             else frequentWords.put(word,1);
         }
-        List<String> candidates = new ArrayList<>(frequentWords.keySet());
+        candidates = new ArrayList<>(frequentWords.keySet());
         Collections.sort(candidates, (w1, w2) -> frequentWords.get(w1).equals(frequentWords.get(w2)) ?
                 w1.compareTo(w2) : frequentWords.get(w2) - frequentWords.get(w1));
         return candidates.subList(0, k);
