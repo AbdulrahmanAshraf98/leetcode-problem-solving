803137cb288e41d4f096cbb7d7ba884d3540c852
Abdulrhman Ashraf <89071774+AbdulrahmanAshraf98@users.noreply.github.com>
2024-07-07 11:10:54 +0300

    Time: 7 ms (68.36%) | Memory: 44.6 MB (41.34%) - LeetSync

---
+++ 692-top-k-frequent-words/top-k-frequent-words.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        HashMap<String,Integer> frequentWords= new HashMap<>();
+        for(String word : words ){
+            if(frequentWords.get(word)!=null)frequentWords.put(word,frequentWords.get(word)+1);
+            else frequentWords.put(word,1);
+        }
+        List<String> candidates = new ArrayList<>(frequentWords.keySet());
+        Collections.sort(candidates, (w1, w2) -> frequentWords.get(w1).equals(frequentWords.get(w2)) ?
+                w1.compareTo(w2) : frequentWords.get(w2) - frequentWords.get(w1));
+
+        return candidates.subList(0, k);
+
+    }
+}
