23ac87df576b44dfd8889eac6bbd09364ff6bebd
Abdulrhman Ashraf <89071774+AbdulrahmanAshraf98@users.noreply.github.com>
2024-07-08 14:25:56 +0300

    Time: 14 ms (26.04%) | Memory: 48.9 MB (92.47%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -6,14 +6,12 @@ class Solution {
             if(!numberFrequenceyApperers.containsKey(num))numberFrequenceyApperers.put(num,1);
             else numberFrequenceyApperers.put(num,numberFrequenceyApperers.get(num)+1);
        }
-       int maxFrequencey=-1;
-       int max=-1;
+ 
        for(int num :numberFrequenceyApperers.keySet()){
-            if(numberFrequenceyApperers.get(num)>maxFrequencey){
-                maxFrequencey= numberFrequenceyApperers.get(num);
-                max= num;
+            if(numberFrequenceyApperers.get(num)>majorityValue){
+                return num;
             }
        }
-       return max ;
+       return 0 ;
     }
 }
