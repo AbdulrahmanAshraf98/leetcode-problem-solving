3a1517c367412574e9392de360bc18774d8064d0
Abdulrhman Ashraf <89071774+AbdulrahmanAshraf98@users.noreply.github.com>
2024-07-08 13:31:25 +0300

    Time: 2 ms (95.18%) | Memory: 46.5 MB (41.27%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.java
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.java
@@ -6,23 +6,7 @@ class Solution {
             nums[i]=maxRightElement;
             maxRightElement=Math.max(maxRightElement,current);
         }
-        // int i = 0;
-        // int j = i+1;
-        // int maxRightElement = -1;
-        // while(i<arr.length){
-        //     if(i==arr.length-1)arr[i]=-1;
-        //     if(j==arr.length){
-        //         arr[i]=maxRightElement;
-        //         i++;
-        //         j=i+1;
-        //         maxRightElement= -1; 
-        //     }
-        //     else{
-        //         maxRightElement=Math.max(arr[j],maxRightElement);
-        //         j++;
-        //     }
-            
-        // }
+
         return nums;
     }
 }
