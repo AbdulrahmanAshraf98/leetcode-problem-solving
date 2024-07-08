3457c6f7f8f15ff89ab6c6c5ce6f84f317eb2419
Abdulrhman Ashraf <89071774+AbdulrahmanAshraf98@users.noreply.github.com>
2024-07-08 13:29:38 +0300

    Time: 2 ms (95.18%) | Memory: 46.3 MB (69.90%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.java
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.java
@@ -1,22 +1,28 @@
 class Solution {
-    public int[] replaceElements(int[] arr) {
-        int i = 0;
-        int j = i+1;
-        int maxRightElement = -1;
-        while(i<arr.length){
-            if(i==arr.length-1)arr[i]=-1;
-            if(j==arr.length){
-                arr[i]=maxRightElement;
-                i++;
-                j=i+1;
-                maxRightElement= -1; 
-            }
-            else{
-                maxRightElement=Math.max(arr[j],maxRightElement);
-                j++;
-            }
-            
+    public int[] replaceElements(int[] nums) {
+         int maxRightElement = -1;
+        for(int i = nums.length-1; i>=0;i--){
+            int current= nums[i];
+            nums[i]=maxRightElement;
+            maxRightElement=Math.max(maxRightElement,current);
         }
-        return arr;
+        // int i = 0;
+        // int j = i+1;
+        // int maxRightElement = -1;
+        // while(i<arr.length){
+        //     if(i==arr.length-1)arr[i]=-1;
+        //     if(j==arr.length){
+        //         arr[i]=maxRightElement;
+        //         i++;
+        //         j=i+1;
+        //         maxRightElement= -1; 
+        //     }
+        //     else{
+        //         maxRightElement=Math.max(arr[j],maxRightElement);
+        //         j++;
+        //     }
+            
+        // }
+        return nums;
     }
 }
