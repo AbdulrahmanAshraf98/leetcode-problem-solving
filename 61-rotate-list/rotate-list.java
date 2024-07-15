da39574edf90b4a871ef7f3daaf2b9ff6a9febe3
Abdulrhman Ashraf <89071774+AbdulrahmanAshraf98@users.noreply.github.com>
2024-07-15 16:23:12 +0300

    Time: 0 ms (100.00%) | Memory: 42.6 MB (17.77%) - LeetSync

---
+++ 61-rotate-list/rotate-list.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+          if (head == null || head.next == null || k == 0) {
+            return head;
+        }
+        ListNode temp = head;
+        int length = 1;
+        while (temp.next != null) {
+            temp = temp.next;
+            length++;
+        }
+        k = k % length; 
+        while(k>0){
+        ListNode slow=head;
+        ListNode fast = head.next;
+        while(fast.next!=null){
+            slow=fast;
+            fast=fast.next;
+        }
+        fast.next = head;
+        slow.next = null;
+        head = fast;
+        k--;
+        }
+        return head;
+    }
+   
+}
