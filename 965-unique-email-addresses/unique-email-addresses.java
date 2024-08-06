ca32ab12939421f189956b9ad644b98cc0dc17e6
Abdulrhman Ashraf <89071774+AbdulrahmanAshraf98@users.noreply.github.com>
2024-08-06 07:33:20 +0300

    Time: 15 ms (58.69%) | Memory: 44.9 MB (47.16%) - LeetSync

---
+++ 965-unique-email-addresses/unique-email-addresses.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numUniqueEmails(String[] emails) {
+        Set<String> uniqueEmails = new HashSet<>();
+        for(String email : emails){
+            String[] parts = email.split("@");
+            if (parts.length != 2) continue;
+            String local = parts[0];
+            String domain = parts[1];
+            if(local.contains("+")) local = local.substring(0 , local.indexOf("+"));
+            local=local.replace(".","");
+            String cleanEmail= local+"@"+domain;
+            uniqueEmails.add(cleanEmail);
+        }
+        return uniqueEmails.size();
+        
+    }
+}
