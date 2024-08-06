96eac288e9a81bb9147d48580889e55c1381ca3c
Abdulrhman Ashraf <89071774+AbdulrahmanAshraf98@users.noreply.github.com>
2024-08-06 07:34:44 +0300

    Time: 15 ms (58.69%) | Memory: 45 MB (47.16%) - LeetSync

--- 965-unique-email-addresses/unique-email-addresses.java
+++ 965-unique-email-addresses/unique-email-addresses.java
@@ -9,7 +9,7 @@ class Solution {
             if(local.contains("+")) local = local.substring(0 , local.indexOf("+"));
             local=local.replace(".","");
             String cleanEmail= local+"@"+domain;
-            uniqueEmails.add(cleanEmail);
+            if(!uniqueEmails.contains(cleanEmail)) uniqueEmails.add(cleanEmail);
         }
         return uniqueEmails.size();
         
