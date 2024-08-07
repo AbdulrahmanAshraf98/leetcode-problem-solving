8baede12422039e39829fe1d2e5272da6edd55a8
Abdulrhman Ashraf <89071774+AbdulrahmanAshraf98@users.noreply.github.com>
2024-08-07 09:37:04 +0300

    Time: 2 ms (28.89%) | Memory: 45.7 MB (6.61%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,12 +1,16 @@
 class Solution {
     public int missingNumber(int[] nums) {
-            Arrays.sort(nums);
-            int arraySize=nums.length;
-            for(int i =0 ; i<arraySize;i++){
-                if(nums[i]!=i){
-                    return i;
-                }
-            }
-            return arraySize;
+            // Arrays.sort(nums);
+            // int arraySize=nums.length;
+            // for(int i =0 ; i<arraySize;i++){
+            //     if(nums[i]!=i){
+            //         return i;
+            //     }
+            // }
+            // return arraySize;
+            int sum = IntStream.of(nums).sum();
+            int n = nums.length;
+            int expectedSum = n * (n + 1) / 2;
+            return expectedSum-sum;
     }
 }
